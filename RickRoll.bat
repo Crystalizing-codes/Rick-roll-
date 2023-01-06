5c4942d266a887a992fc33e40df7ebe1bb9fc6e9
Crystalizing-codes <121509261+Crystalizing-codes@users.noreply.github.com>
2023-01-06 10:25:24 +0400

    Rick Roll .exe
    Opens up a new tab in your browser and plays the rick roll meme.

---
+++ RickRoll.bat
@@ -0,0 +1,9 @@
+@echo off
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+:start
+sleep 10
+start "youtube" "https://www.youtube.com/watch?v=iik25wqIuFo"
+set choice=
+set /p choice="Do you want to run the virus again. press y for Yes:"
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='y' goto start
